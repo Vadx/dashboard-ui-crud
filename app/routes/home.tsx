3eced591dc65f3b1149bcebbaf60f996be916d08
Vadx <lv_km@ukr.net>
2025-11-29 16:30:47 +0200

    feat: implement HomeCards component with navigation cards for Products and Users

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import HomeCards from "@/components/home-cards";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -8,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <>Home</>;
+  return <HomeCards />;
 }
